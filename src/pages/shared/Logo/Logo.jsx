c13d9d39f97ebf776a73776f2762bee7ead1bcf7
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-05-10 23:49:24 +0600

    designed navbar

---
+++ src/pages/shared/Logo/Logo.jsx
@@ -0,0 +1,7 @@
+import img from "../../../assets/logo.png";
+
+function Logo() {
+  return <img className="w-40" src={img} alt="" />;
+}
+
+export default Logo;
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,7 +1,65 @@
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../Logo/Logo";
+
 function Navbar() {
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/add-book">Add Book</NavLink>
+      </li>
+      <li>
+        <NavLink to="/all-books">All Books</NavLink>
+      </li>
+      <li>
+        <NavLink to="/borrowed-books">Borrowed Books</NavLink>
+      </li>
+    </>
+  );
+
   return (
-    <div>
-      <h1 className="text-4xl">This is Navbar</h1>
+    <div className="bg-base-300">
+      <div className="navbar container mx-auto h-24 px-4">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 gap-1"
+            >
+              {links}
+            </ul>
+          </div>
+          <Link to="/">
+            <Logo />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 space-x-1">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <Link to="login" className="btn">
+            Login
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
