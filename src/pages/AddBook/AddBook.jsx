10376f530380cadedc01bd360edd2aa8ffd348ef
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-05-11 14:38:05 +0600

    added the add book pages content

--- src/pages/AddBook/AddBook.jsx
+++ src/pages/AddBook/AddBook.jsx
@@ -1,9 +1,129 @@
-function AddBook() {
+import { useState } from "react";
+
+const AddBook = () => {
+  const [name, setName] = useState(null);
+  const [quantity, setQuantity] = useState(null);
+  const [author, setAuthor] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [rating, setRating] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const bookInfo = {
+      name,
+      quantity,
+      author,
+      category,
+      description,
+      rating,
+    };
+    console.log(bookInfo);
+  };
+
   return (
-    <div>
-      <h1 className="text-4xl">This is Add Book Page</h1>
+    <div className="flex justify-center items-center">
+      <div className=" w-full p-6 bg-base-300 shadow-md rounded-lg">
+        <h1 className="text-3xl font-semibold mb-6 text-center text-gray-800">
+          Add Book
+        </h1>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="grid md:grid-cols-2 gap-4">
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Name
+              </label>
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Quantity
+              </label>
+              <input
+                type="number"
+                className="input input-bordered w-full"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Author
+              </label>
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                required
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Category
+              </label>
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                required
+              />
+            </div>
+            <div className="md:col-span-2">
+              <label className="block text-sm font-medium text-gray-700">
+                Rating
+              </label>
+              <input
+                type="number"
+                min="1"
+                max="5"
+                className="input input-bordered w-full"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="col-span-2">
+              <label className="block text-sm font-medium text-gray-700">
+                Description
+              </label>
+              <textarea
+                className="textarea textarea-bordered w-full"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-primary w-full py-3 rounded-lg"
+          >
+            Add
+          </button>
+        </form>
+        <div className="mt-8 text-center">
+          <h2 className="text-xl font-semibold mb-2 text-gray-800">
+            About the Book
+          </h2>
+          <p className="text-gray-700">
+            Feel free to add the information or details about the book here.
+          </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default AddBook;
